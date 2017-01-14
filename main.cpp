6f11106ca83ab6b378b6fcb7c00b6dd8509c4f2c
wsaicyj <wsaicyj@gmail.com>
2017-01-14 15:53:10 +0800

    Cproject-02

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Enter two number:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+//    std::cin.get();
     return 0;
 }
