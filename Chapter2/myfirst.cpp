278d5121b08663d8d6f4649d2d5dbd618a1272ef
wsaicyj <wsaicyj@gmail.com>
2017-01-17 23:58:28 +0800

    Chapter2-2.2

---
+++ Chapter2/myfirst.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Aaron on 2017/1/17.
+//
+
+#include <iostream>
+
+int main(){
+    using namespace std;
+    cout << "Come up and C++ me some time.\n";
+//    cout << endl;
+    cout << "You wont't regret it!" << endl;
+    return 0;
+}
